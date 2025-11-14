39e452a6b5a3acc5e54d84cc2e4e71aea425d8e0
Chaib Mohamed <163442102+sci-chaib@users.noreply.github.com>
2025-11-14 06:12:23 +0000

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
-# File: guessinggame.sh
 
-function guessinggame(){
-    no_of_files=$(pwd | ls | wc -l)
-    while true;
-    do
-        echo "pleas enter your guess"
-        read  number
-        if [ $number -lt $no_of_files ]
-        then
-            echo "Your guess is less than the number of files"
-        elif [ $number -gt $no_of_files ]
-        then
-            echo "Your guess is greater than the number of files"
-        else
-            echo "Congratulation,your guess is correct!"
-        break;
-        fi
-    done
+count_files() {
+    ls -1 | wc -l
 }
 
-echo "Guess how many files are there in current directory?"
-guessinggame
+correct=$(count_files)
+
+echo "Guess how many files are in the current directory:"
+read guess
+
+while true
+do
+    if [[ $guess -eq $correct ]]; then
+        echo "Correct."
+        break
+    fi
+
+    if [[ $guess -lt $correct ]]; then
+        echo "Too low."
+    else
+        echo "Too high."
+    fi
+
+    read guess
+done
